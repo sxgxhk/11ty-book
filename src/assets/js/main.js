4fcfeca75f209511f7beaa4ea978abd88c428d00
rebron1900 <me@1900.live>
2024-03-28 21:56:17 +0800

    使用artalklite和quicklink本地化，启用netlify的gzip

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,34 +1,51 @@
-import '../sass/book.scss';
+import "../sass/book.scss";
 
-import Alpine from 'alpinejs'
-import mediumZoom from 'medium-zoom';
-import search from './search';
+// import Alpine from "alpinejs";
+import mediumZoom from "medium-zoom";
+import search from "./search";
+import Artalk from './ArtalkLite';
+import quicklink from 'quicklink/dist/quicklink.umd'
 
+// window.Alpine = Alpine;
 
-window.Alpine = Alpine
-
-Alpine.start()
+// Alpine.start();
 
 //init kg-gallery-image
 var gallery = document.querySelectorAll(".kg-gallery-image img");
-gallery.forEach(function(e) {
-    var l = e.closest(".kg-gallery-image")
-      , a = e.attributes.width.value / e.attributes.height.value;
-    l.style.flex = a + " 1 0%"
+gallery.forEach(function (e) {
+    var l = e.closest(".kg-gallery-image"),
+        a = e.attributes.width.value / e.attributes.height.value;
+    l.style.flex = a + " 1 0%";
 });
 
-const images = document.querySelectorAll('.markdown img');
-  mediumZoom(images, {
-    background: 'rgba(0,0,0,0.75)',
-    container: '.medium-zoom-overlay'
+const images = document.querySelectorAll(".markdown img");
+mediumZoom(images, {
+    background: "rgba(0,0,0,0.75)",
+    container: ".medium-zoom-overlay",
 });
-
 
 search();
 
-
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("sw.js", { scope: "/" }
-  );
+    navigator.serviceWorker.register("sw.js", { scope: "/" });
 }
 
+if (
+    commentinfo.type == "artalk" &&
+    document.getElementById("comments") != null
+) {
+    const artalk = Artalk.init({
+        el: commentinfo.el, // 绑定元素的 Selector
+        server: commentinfo.server, // 后端地址
+        site: commentinfo.name, // 你的站点名
+    });
+    artalk.on("list-loaded", () => {
+        artalk.setDarkMode(
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? true
+                : false
+        );
+    });
+}
+
+window.addEventListener('load', () => {  quicklink.listen();});
