1aa5cb6349dfcc6ae04cba7f78de018fa91085e3
rebron1900 <me@1900.live>
2024-03-28 10:42:54 +0800

    modified:   package.json 	modified:   rollup.config.mjs 	modified:   src/site/_includes/partials/header.liquid 	modified:   src/site/index.liquid 	modified:   src/site/page.liquid 	modified:   src/site/single.liquid 	modified:   yarn.lock

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,23 +2,24 @@ import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import postcss from "rollup-plugin-postcss";
 import babel from "@rollup/plugin-babel";
-import terser from "@rollup/plugin-terser";
 import atImport from "postcss-import";
 import postcssPresetEnv from "postcss-preset-env";
-import { resolve } from "path";
-import livereload from "rollup-plugin-livereload";
 import copy from "rollup-plugin-copy";
+import terser from "@rollup/plugin-terser";
+import del from 'rollup-plugin-delete'
+
+const sourceMap = process.env.NODE_ENV.trim() === 'dev';
 
 export default {
     input: "src/assets/js/main.js",
-    output: { dir: "dist/assets/", sourcemap: true, format: "es" },
+    output: { dir: "dist/assets/", sourcemap: sourceMap, format: "es" },
     plugins: [
         nodeResolve(),
         commonjs(),
         babel({ babelHelpers: "bundled" }),
         postcss({
             extract: true,
-            sourceMap: true,
+            sourceMap: sourceMap,
             extensions: ["css", "scss"],
             plugins: [
                 atImport(),
@@ -29,13 +30,8 @@ export default {
                     },
                 }),
             ],
-            //minimize: true,
+            minimize: true,
         }),
-        // livereload({
-        //     watch: resolve("."),
-        //     extraExts: ["hbs"],
-        //     exclusions: [resolve("node_modules")],
-        // }),
         copy({
             targets: [
                 { src: "src/assets/fonts", dest: "dist/assets/" },
@@ -43,6 +39,7 @@ export default {
                 { src: "src/assets/svg", dest: "dist/assets/" },
             ],
         }),
-        //terser(),
+        terser(),
+        del({ targets: 'dist/*' })
     ],
 };
