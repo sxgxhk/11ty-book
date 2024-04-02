6cde0e06a5bfb39a99e0474a42835c8bae39b8ba
rebron1900 <me@1900.live>
2024-04-02 15:04:26 +0800

    Changes to be committed: 	modified:   src/assets/js/main.js 	modified:   src/assets/sass/_main.scss 	modified:   src/site/_includes/partials/menu-theme.liquid 	modified:   src/site/memos.liquid

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,36 +6,34 @@ import search from "./search";
 import Artalk from "./ArtalkLite";
 import quicklink from "quicklink/dist/quicklink.umd";
 
-import "./highlightjs-badge.min.js"
-import { getMemos,parseMemos } from "./memos.js";
-
+import "./highlightjs-badge.min.js";
+import { getMemos, parseMemos } from "./memos.js";
 
 window.Alpine = Alpine;
-Alpine.data('theme', () => ({
-    themeName:localStorage.name,
-    changeTheme:changeTheme,
-    setName: function(){
-        this.themeName = localStorage.name
-    }
-}))
-Alpine.data('memos', () => ({
-    data:{},
-    limit:0,
-    offset:0,
-    url:'',
-    getMemoss:function(){
-        this.data = getMemos(this.url,this.limit,this.offset);
+Alpine.data("theme", () => ({
+    themeName: localStorage.name,
+    changeTheme: changeTheme,
+    setName: function () {
+        this.themeName = localStorage.name;
     },
-    loadmore:function(){
+}));
+Alpine.data("memos", () => ({
+    data: {},
+    limit: 0,
+    offset: 0,
+    url: "",
+    getMemoss: function () {
+        this.data = getMemos(this.url, this.limit, this.offset);
+    },
+    loadmore: function () {
         this.offset = this.offset + this.offset;
         this.limit = this.limit + this.limit;
-        console.log(this.getMemoss())
-    }
-}))
+        console.log(this.getMemoss());
+    },
+}));
 
 Alpine.start();
 //init kg-gallery-image
-
 
 var gallery = document.querySelectorAll(".kg-gallery-image img");
 gallery.forEach(function (e) {
@@ -67,33 +65,54 @@ if (
         server: commentinfo.server, // 后端地址
         site: commentinfo.name, // 你的站点名
     });
-    artalk.on("list-loaded",changeTheme(localStorage.theme,localStorage.name));
+    artalk.on(
+        "list-loaded",
+        changeTheme(localStorage.theme, localStorage.name)
+    );
 }
 
 window.addEventListener("load", () => {
     quicklink.listen();
 });
 
-function changeTheme(theme,name) {
-    if(theme == 'auto') {
-        document.documentElement.setAttribute("class","");
-        const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        if(window.artalk) window.artalk.setDarkMode(prefersDarkScheme)
-    }else{
+function changeTheme(theme, name) {
+    if (theme == "auto") {
+        document.documentElement.setAttribute("class", "");
+        const prefersDarkScheme = window.matchMedia(
+            "(prefers-color-scheme: dark)"
+        ).matches;
+        if (window.artalk) window.artalk.setDarkMode(prefersDarkScheme);
+    } else {
         // 切换主题并存储到localStorage
         document.documentElement.setAttribute("class", theme);
-        if(window.artalk) window.artalk.setDarkMode(theme === "dark" ? true:false)
+        if (window.artalk)
+            window.artalk.setDarkMode(theme === "dark" ? true : false);
     }
     localStorage.theme = theme;
     localStorage.name = name;
 }
 
-var options = {   // optional
+var options = {
+    // optional
     contentSelector: ".markdown",
-    copyiconclass:"test",
-    copyIconContent: "<img src='"+ location.origin +"/assets/svg/theme.svg' class='book-icon' /></span>"
- };
+    copyiconclass: "test",
+    copyIconContent:
+        "<img src='" +
+        location.origin +
+        "/assets/svg/theme.svg' class='book-icon' /></span>",
+};
 
 window.highlightJsBadge(options);
 
-
+var options = { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 };
+function success(pos) {
+    var crd = pos.coords;
+    console.log("Your current position is:");
+    console.log("Latitude : " + crd.latitude);
+    console.log("Longitude: " + crd.longitude);
+    console.log("More or less " + crd.accuracy + " meters.");
+}
+function error(err) {
+    console.warn("ERROR(" + err.code + "): " + err.message);
+}
+navigator.geolocation.getCurrentPosition(success, error, options);
